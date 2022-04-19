92ff64725b51f207b5dad52c664c473ccc7383d1
Jaqb-Janicqi <janicus321@gmail.com>
2022-04-19 19:58:53 +0200

    message

---
+++ world.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class World
+{
+    vector<vector<char>> board;
+    int x;
+    int y;
+    World(int x, int y)
+    {
+
+    }
+    
+};
+
+int main()
+{   
+
+    return 0;
+}
